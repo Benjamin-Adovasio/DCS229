efa0686d279b8a237c06c23e7309b459f4e25025
Benjamin Adovasio <benjamin@adovasio.com>
2026-01-16 09:57:25 -0500

    hello_world

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+#Function that takes in a list of numbers and returns the sum of all numbers
+def numSum(numList):
+    sum = 0
+    for num in numList:
+        sum += num
+    return sum
