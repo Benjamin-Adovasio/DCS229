470bbb28a1eb12a3a095b5a76e0000232975b4dd
Benjamin Adovasio <benjamin@adovasio.com>
2026-02-10 18:43:12 -0500

    added method for interation

--- Project 1/utilities.py
+++ Project 1/utilities.py
@@ -1,2 +1,10 @@
 from __future__ import annotations
 from Card import Card
+
+def sum_cards_iter(cards: list[Card]) -> int:
+    total = 0
+    for card in cards:
+        total = total + card
+    return total
+#Added for project 2: Created a function to sum a list of Card objects using iteration.
+
