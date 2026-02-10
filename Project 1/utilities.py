86c29cce1b2e654ecf38ef10cb8f3c2de5d72b86
Benjamin Adovasio <benjamin@adovasio.com>
2026-02-10 18:45:17 -0500

    added tests

--- Project 1/utilities.py
+++ Project 1/utilities.py
@@ -13,3 +13,14 @@ def sum_cards_recursive(cards: list[Card]) -> int:
         return 0
     return cards[0] + sum_cards_recursive(cards[1:])
 #Added for project 2: Created a function to sum a list of Card objects using recursion.
+
+def test_sum_cards():
+    cards = [
+        Card("diamond", 5),
+        Card("clubs", 3),
+        Card("spade", 12),
+        Card("clubs", 1)
+    ]
+    assert sum_cards_iter(cards) == 21
+    assert sum_cards_recursive(cards) == 21
+#Added for project 2: Example tests given
