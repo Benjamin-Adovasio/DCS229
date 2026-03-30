97bdfd19076199ceb3809f4ab4094a5e35a27475
Benjamin Adovasio <benjamin@adovasio.com>
2026-03-30 09:16:47 -0400

    added imports

---
+++ Project 5/TurtleDFS.py
@@ -0,0 +1,11 @@
+######################################
+# DCS 229 -- TurtleDFS
+# Depth-First Search implementation for Turtle Maze
+# Date: 03/29/2026
+# Name: Benjamin Adovasio
+# Resources Used: I worked with Pat Cohen on the project.
+##########################################
+
+import os
+from Stack import Stack
+from TurtleMaze import TurtleMaze, Cell, Contents
